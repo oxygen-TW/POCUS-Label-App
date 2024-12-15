34ba369f06c332a63c4fac58abaf2c799811b167
oxygen-TW <howard2805@gmail.com>
2024-12-15 14:13:42 +0800

    加入專案檔案。

---
+++ LabelApp/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace LabelApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BackBtn = new System.Windows.Forms.Button();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.goto_box1 = new System.Windows.Forms.ComboBox();
+            this.AnsLabel = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(527, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "High Quality";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Crimson;
+            this.button2.Location = new System.Drawing.Point(527, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 55);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Low Quality";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BackBtn
+            // 
+            this.BackBtn.Enabled = false;
+            this.BackBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BackBtn.Location = new System.Drawing.Point(6, 41);
+            this.BackBtn.Name = "BackBtn";
+            this.BackBtn.Size = new System.Drawing.Size(114, 48);
+            this.BackBtn.TabIndex = 3;
+            this.BackBtn.Text = "< Back";
+            this.BackBtn.UseVisualStyleBackColor = true;
+            this.BackBtn.Click += new System.EventHandler(this.back_Click);
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NextBtn.Location = new System.Drawing.Point(126, 41);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(118, 48);
+            this.NextBtn.TabIndex = 4;
+            this.NextBtn.Text = "Next >";
+            this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.next_Click);
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IdLabel.Location = new System.Drawing.Point(521, 12);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(48, 24);
+            this.IdLabel.TabIndex = 5;
+            this.IdLabel.Text = "ID=0";
+            // 
+            // goto_box1
+            // 
+            this.goto_box1.FormattingEnabled = true;
+            this.goto_box1.Location = new System.Drawing.Point(6, 121);
+            this.goto_box1.Name = "goto_box1";
+            this.goto_box1.Size = new System.Drawing.Size(238, 21);
+            this.goto_box1.TabIndex = 6;
+            this.goto_box1.SelectedValueChanged += new System.EventHandler(this.goto_box1_SelectedValueChanged);
+            // 
+            // AnsLabel
+            // 
+            this.AnsLabel.AutoSize = true;
+            this.AnsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AnsLabel.Location = new System.Drawing.Point(610, 12);
+            this.AnsLabel.Name = "AnsLabel";
+            this.AnsLabel.Size = new System.Drawing.Size(78, 24);
+            this.AnsLabel.TabIndex = 7;
+            this.AnsLabel.Text = "QC=N/L";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(481, 389);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Enabled = false;
+            this.saveBtn.Location = new System.Drawing.Point(690, 378);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(87, 23);
+            this.saveBtn.TabIndex = 8;
+            this.saveBtn.Text = "Export Result";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.BackBtn);
+            this.groupBox1.Controls.Add(this.NextBtn);
+            this.groupBox1.Controls.Add(this.goto_box1);
+            this.groupBox1.Location = new System.Drawing.Point(527, 209);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 161);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Go to:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 411);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.AnsLabel);
+            this.Controls.Add(this.IdLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "LUS QC Label App";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button BackBtn;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.Label IdLabel;
+        private System.Windows.Forms.ComboBox goto_box1;
+        private System.Windows.Forms.Label AnsLabel;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LabelApp/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace LabelApp
+{
+    public partial class Form1 : Form
+    {
+        private int current_id;
+        private string rootPath = ".\\output\\";
+        private string TempFile = ".\\temp";
+        private Dictionary<int, int> videoList;
+        private int totalCount = 123;
+
+        public Form1()
+        {
+            InitializeComponent();
+            videoList = GenerateDict(totalCount);
+            Console.WriteLine(videoList.Count);
+
+            for (int i = 0; i <= totalCount; i++)
+            {
+                goto_box1.Items.Add(i);
+            }
+
+            current_id = 0;
+
+            if (File.Exists(TempFile))
+            {
+                DialogResult res;
+                res = MessageBox.Show("有暫存的進度，是否載入先前的結果?", "繼續", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (res == DialogResult.Yes)
+                {
+                    var results = File.ReadAllText(TempFile);
+                    videoList = JsonConvert.DeserializeObject<Dictionary<int, int>>(results);
+                    Console.WriteLine("Load json.");
+                    updateStatusLabel();
+                }
+                else
+                {
+                    File.Delete(TempFile);
+                    Console.WriteLine("Temp file has been deleted.");
+                }
+            }
+            else
+            {
+                //尚未執行過
+                string info_msg = "您好，非常感謝您協助本研究的進行！本研究旨在利用電腦視覺技術，對肺部POCUS影像進行COVID-19的輔助診斷。然而，我們目前使用的公開資料集品質不穩定，因此誠請您協助進行資料集品質的標註。\r\n\r\n" +
+                    "本次資料集共包含124個影片，請您協助判讀這些超音波影片是否為高品質（合格）資料。合格的標準為：假設該超音波影片屬於您的患者，且其主訴與呼吸系統相關，此影片應具有足夠的資訊和正確的解剖定位，以提供診斷線索。\r\n\r\n" +
+                    "再次感謝您的協助，若標記途中有任何疑問，隨時歡迎與我聯絡\n" +
+                    "r12945029@ntu.edu.tw 劉子豪";
+
+                MessageBox.Show(info_msg, "說明", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+        }
+        private static Dictionary<int, int> GenerateDict(int totalCount)
+        {
+            Dictionary<int, int> fileDictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < totalCount+1; i++)
+            {
+                fileDictionary[i] = -1;
+            }
+
+            return fileDictionary;
+        }
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+            Console.WriteLine((rootPath + current_id.ToString() + ".mp4").Trim());
+            axWindowsMediaPlayer1.URL = rootPath + current_id.ToString() + ".mp4";
+        }
+
+        public static void WriteDictionaryToJsonFile<T, U>(Dictionary<T, U> dictionary, string filePath)
+        {
+            // 將字典轉換為 JSON 字符串，使用縮排格式
+            string jsonString = JsonConvert.SerializeObject(dictionary, Newtonsoft.Json.Formatting.Indented);
+
+            // 將 JSON 字符串寫入檔案
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        private void updateStatusLabel()
+        {
+            string BaseText = "Label=";
+            if (videoList[current_id] == -1)
+            {
+                AnsLabel.Text = BaseText + "N/L";
+            }
+            else
+            {
+                Console.WriteLine(videoList[current_id]);
+                AnsLabel.Text = BaseText + (videoList[current_id] == 1 ? "High Quality": "Low Quality");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            videoList[current_id] = 1;
+            updateStatusLabel();
+            checkFinished();
+            save_to_tempfile();
+        }
+
+        private void checkEnd()
+        {
+            BackBtn.Enabled = current_id - 1 < 0 ? false : true;
+            NextBtn.Enabled = current_id + 1 > totalCount ? false : true;
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            current_id--;
+            IdLabel.Text = "ID=" + current_id.ToString();
+            axWindowsMediaPlayer1.URL = rootPath + current_id.ToString() + ".mp4";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            updateStatusLabel();
+            checkFinished();
+            checkEnd();
+
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            current_id ++;
+            IdLabel.Text = "ID=" + current_id.ToString();
+            axWindowsMediaPlayer1.URL = rootPath + current_id.ToString() + ".mp4";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            updateStatusLabel();
+            checkFinished();
+            checkEnd();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            videoList[current_id] = 0;
+            updateStatusLabel();
+            checkFinished();
+            save_to_tempfile();
+        }
+
+        private void goto_box1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            current_id = (int)goto_box1.SelectedItem;
+            IdLabel.Text = "ID=" + current_id.ToString();
+            axWindowsMediaPlayer1.URL = rootPath + current_id.ToString() + ".mp4";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            updateStatusLabel();
+            checkFinished();
+            checkEnd();
+        }
+
+        private void checkFinished()
+        {
+            if (videoList.ContainsValue(-1))
+            {
+                saveBtn.Enabled = false;
+            }
+            else
+            {
+                saveBtn.Enabled = true;
+            }
+        }
+        private void saveBtn_Click(object sender, EventArgs e)
+            {SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Title = "存檔",
+                    FileName = "QC_label",          // 設定預設檔名
+                    Filter = "JSON files (*.json)|*.json", // 限制副檔名為 .json
+                    DefaultExt = "json",             // 預設副檔名
+                    AddExtension = true              // 自動添加副檔名
+                };
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    Console.WriteLine("選擇的存檔路徑: " + filePath);
+                    WriteDictionaryToJsonFile(videoList, filePath);
+                    MessageBox.Show("存檔成功", "完成", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    Console.WriteLine("存檔取消");
+                MessageBox.Show("存檔取消", "取消", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            }
+
+        private void save_to_tempfile()
+        {
+            WriteDictionaryToJsonFile(videoList, TempFile);
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
